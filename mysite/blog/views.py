ee993c78a3613b202061c3f7e8f97cf42dba7b2b
igorkang-smc <ikanv13@gmail.com>
2025-08-11 20:34:25 +0900

    feat: implement full-text search of posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,11 +2,16 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Post
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    TrigramSimilarity
+)
 
 
 # Create your views here.
@@ -35,6 +40,7 @@ def post_list(request, tag_slug=None):
             'tag': tag
         }
     )
+
 
 class PostListView(ListView):
     """
@@ -146,5 +152,35 @@ def post_comment(request, post_id):
             'post': post,
             'form': form,
             'comment': comment
+        }
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                'title', weight='A'
+            ) + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity('title', query),
+                )
+                .filter(similarity__gt=0.1)
+                .order_by('-similarity')
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
