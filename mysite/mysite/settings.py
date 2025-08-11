b1f714e42c45929c273cd658d0cd23e70503af87
igorkang-smc <ikanv13@gmail.com>
2025-08-11 19:03:41 +0900

    feat: add sitemap feature

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
     'taggit',
     'blog.apps.BlogConfig'
@@ -130,3 +132,4 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+SITE_ID = 1
